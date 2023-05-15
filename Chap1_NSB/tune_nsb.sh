46cf779216ed50914d6785e892c195803146e0aa
Kenta2076 <68601885+Kenta2076@users.noreply.github.com>
2023-05-15 17:03:06 +0900

    Update tune_nsb.sh

--- Chap1_NSB/tune_nsb.sh
+++ Chap1_NSB/tune_nsb.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
+#
+# usage: lstchain_tune_nsb [-h] --config CONFIG --input-mc INPUT_MC --input-data INPUT_DATA [--output-file OUTPUT_FILE] [--overwrite]
+#
+#
 
-# 
+# Input MC file. Must be a simtel file! 
 MCFILE=/fefs/aswg/data/mc/DL0/LSTProd2/TrainingDataset/Protons/dec_2276/sim_telarray/node_theta_44.927_az_85.716_/output_v1.4/simtel_corsika_theta_44.927_az_85.716_run1.simtel.gz
 
+# Input real DL1 file. 
 DL1DATA=`ls /fefs/aswg/data/real/DL1/20*/v*/tailcut84/dl1_LST-1.Run09686.0000.h5`
 
-CONFIG=/fefs/aswg/workspace/seiya.nozaki/data/GRB221009A/20221105_nsb_tune/DL1_MC/lstchain_standard_config.json
+# Config file. Must include "image_modifier" section in this config file!
+CONFIG=/home/kenta.terauchi/Work/oda2023/standard_config.json
 
-
+# Outout config file
+OUTPUTPATH=/home/kenta.terauchi/Work/oda2023/output_standard_config.json
 
 lstchain_tune_nsb \
 --input-mc ${MCFILE} \
 --input-data ${DL1DATA} \
---config ${CONFIG}
+--config ${CONFIG} \
+--output-file ${OUTPUTPATH} \
+--overwrite
